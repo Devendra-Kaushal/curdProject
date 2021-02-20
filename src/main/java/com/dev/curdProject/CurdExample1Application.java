725dfd0a03452eafcc823594161811891e33fb19
Devendra Kaushal <devendraskaushal@gmail.com>
2021-02-20 12:44:30 +0530

    Initial commit

---
+++ src/main/java/com/dev/curdProject/CurdExample1Application.java
@@ -0,0 +1,13 @@
+package com.dev.curdProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurdExample1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurdExample1Application.class, args);
+	}
+
+}
