ff20097f7f1ca13879171785bb4dc3cd32c32d4c
Devendra Kaushal <devendraskaushal@gmail.com>
2021-02-24 23:37:24 +0530

    added aop files

---
+++ src/main/java/com/dev/curdProject/WebConfig.java
@@ -0,0 +1,20 @@
+package com.dev.curdProject;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.dev.curdProject.aop.LoggingInterceptor;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer{
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		
+		registry.addInterceptor(new LoggingInterceptor());
+	}
+	
+	
+
+}
